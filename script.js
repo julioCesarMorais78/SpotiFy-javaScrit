5aa4403b34a42f3622119ccb3adb5802b2a611d2
julioCesarMorais78 <juliocesarmorais78@gmail.com>
2025-07-10 20:40:38 -0300

    Inserindo mecanismo Pause Play no js

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+// https://www.youtube.com/watch?v=q__yopYUW80&t=438s
 // console.log("Consolelog message");
 const songname = document.getElementById("song-name"); //id para a teg que mostra o nome da música
 const song = document.getElementById("audio"); // id da tag de audio que aponta para o arquivo mp3
 const play = document.getElementById("play") // id aplicado ao botão de play
 
 songname.innerText = "The Pretender";
+let isPlaying = false;
 // song.pause();
 
 function playSong() {
-    // play.querySelector(".bi").;
+    play.querySelector(".bi").classList.remove("bi-play-circle-fill");
+    play.querySelector(".bi").classList.add("bi-pause-circle-fill");
     song.play();
+    isPlaying = true;
 }
 
-play.addEventListener("click", playSong)
+function pauseSong() {
+    play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
+    play.querySelector(".bi").classList.add("bi-play-circle-fill");
+    song.pause();
+    isPlaying = false
+}
+
+function playPauseDecider() {
+    if (isPlaying === true){
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+play.addEventListener("click", playPauseDecider);
