63b6db00dc81ef253ccc348e52803582fe3da9ab
julioCesarMorais78 <juliocesarmorais@gmail.com>
2025-07-19 18:48:42 -0300

    Criando a mecanica do botão de like com localstorage

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ https://pages.hashtagtreinamentos.com/minicurso-javascript-curso-basico-javascri
 
 editor.quickSuggestions =  (Other, Coments, Strings - off)
 Reiniciar o VSCode
+
+....................................................................
+para ver o armazenamento dos likes no navegador basta.
+Apertar F12
+Application
+Abrir a aba na lateral esquerda de - local storage
+e clicar na linha que aparece o IP que esta rodando a sua aplicação
--- script.js
+++ script.js
@@ -13,24 +13,29 @@ const shuffleButton = document.getElementById("shuffle");
 const repeatButton = document.getElementById("repeat");
 const songTime = document.getElementById("song-time");
 const totalTime = document.getElementById("total-time");
+const likeButton = document.getElementById("like");
+
 
 const The_Pretender = {
     songName : "The Pretender",
     artist : "Foo Fighters",
-    file : "The_Pretender"
+    file : "The_Pretender",
+    liked: false
 }
 const Blaze_of_Glory = {
     songName : "Blaze of Glory",
     artist : "Bon Jovi",
-    file : "Blaze_of_Glory"
+    file : "Blaze_of_Glory",
+    liked: false
 }
 const Behind_Blue_Eyes = {
     songName : "Behind Blue Eyes",
     artist : "Limpbzkit",
-    file : "Behind_Blue_Eyes"
+    file : "Behind_Blue_Eyes",
+    liked: false
 }
 
-const originalPlayList = [The_Pretender, Blaze_of_Glory, Behind_Blue_Eyes];
+const originalPlayList = JSON.parse(localStorage.getItem("playlist")) ?? [The_Pretender, Blaze_of_Glory, Behind_Blue_Eyes];
 let sortedPlaylist = [...originalPlayList];
 
 let index = 0;
@@ -64,12 +69,26 @@ function playPauseDecider() {
     }
 }
 
+function likeButtonRender () {
+    if (sortedPlaylist[index].liked === true) {
+        likeButton.querySelector(".bi").classList.remove("bi-heart");
+        likeButton.querySelector(".bi").classList.add("bi-heart-fill");
+        likeButton.classList.add("button-active");
+    } else {
+        likeButton.querySelector(".bi").classList.remove("bi-heart-fill");
+        likeButton.querySelector(".bi").classList.add("bi-heart");
+        likeButton.classList.remove("button-active");
+    }
+}
+
+
+
 function initializeSong () {
     cover.src = `imagens/${sortedPlaylist[index].file}.jpg`;
     song.src = `songs/${sortedPlaylist[index].file}.mp3`;
     songname.innerHTML = sortedPlaylist[index].songName;
     bandname.innerHTML = sortedPlaylist[index].artist;
-
+    likeButtonRender()
 }
 
 initializeSong();
@@ -174,8 +193,15 @@ function updateTotalTime () {
     totalTime.innerText = toHHMMSS(song.duration);
 }
 
-
-
+function likeButtonClicked () {
+    if (sortedPlaylist[index].liked === false){
+        sortedPlaylist[index].liked = true;
+    } else {
+        sortedPlaylist[index].liked = false;
+    }
+    likeButtonRender();
+    localStorage.setItem("playlist", JSON.stringify(originalPlayList));
+}
 
 play.addEventListener("click", playPauseDecider);
 previous.addEventListener("click", previousSong);
@@ -186,3 +212,4 @@ song.addEventListener("loadedmetadata", updateTotalTime);
 progressContainer.addEventListener("click", jumpTo);
 shuffleButton.addEventListener("click", shuffleButtonClicked);
 repeatButton.addEventListener("click", repeatButtonClicked);
+likeButton.addEventListener("click", likeButtonClicked);
